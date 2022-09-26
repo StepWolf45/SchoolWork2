1cf01daa0c6ed4b2684cbe59254d4838b441e5e0
Fr0kes <85014298+StepWolf45@users.noreply.github.com>
2022-09-26 14:37:03 +0300

    Add files via upload

---
+++ middle.h
@@ -0,0 +1,19 @@
+#ifndef MIDDLE_H
+#define MIDDLE_H
+
+#include <iostream>
+void itc_num_print(int number);
+int itc_len_num(long long number);
+int itc_sum_num(long long number);
+long long itc_multi_num(long long number);
+int itc_max_num(long long number);
+int itc_min_num(long long number);
+long long itc_rev_num(long long number);
+int itc_null_count(long long number);
+bool itc_mirror_num(long long number);
+int itc_mirror_count(long long number);
+int itc_second_max_num(long long number);
+int itc_second_simple_max_num(long long number);
+long long itc_bin_num(long long number);
+
+#endif

837f9c09f4f7a284ba8d0293a76848e493664967
Fr0kes <85014298+StepWolf45@users.noreply.github.com>
2022-10-02 20:03:45 +0300

    Add files via upload

--- Reverse.cpp
+++ Reverse.cpp
@@ -8,8 +8,10 @@ void itc_num_print(int number) {
 	cout << number;
 }
 
-long long itc_rev_num(long long number) {
+long long reverse(long long number) {
 	long long newnum = 0;
+	if (number < 0)
+		number *= -1;
 	while (number > 0) {
 		newnum = newnum * 10 + number % 10;
 		number /= 10;
@@ -17,15 +19,18 @@ long long itc_rev_num(long long number) {
 	return newnum;
 
 }
+long long itc_rev_num(long long number) {
+	return itc_len_num(reverse(number));
+}
 
 bool itc_mirror_num(long long number) {
-	int revnum = itc_rev_num(number);
+	int revnum = reverse(number);
 	return revnum == number;
 }
 
 int itc_mirror_count(long long number) {
 	int kol = 0;
-	for (int i = 1; i <= number; i++) {
+	for (long long i = 1; i <= number; i++) {
 		if (itc_mirror_num(i)) {
 			kol++;
 		}

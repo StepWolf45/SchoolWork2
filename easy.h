09f4fc409d7667872bfce4bdad3239f4f89e3311
Fr0kes <85014298+StepWolf45@users.noreply.github.com>
2022-10-02 20:05:48 +0300

    Add files via upload

---
+++ easy.h
@@ -0,0 +1,25 @@
+#ifndef EASY_H_EASY_H
+#define EASY_H_EASY_H
+
+#include <iostream>
+
+void itc_name();
+void itc_fio();
+int itc_abs(int num);
+double itc_fabs(double num);
+int itc_revnbr(int num);
+bool itc_iseven(int num);
+int itc_max(int num1, int num2);
+int itc_min(int min1, int min2);
+double itc_fmax(double num, double num2);
+double itc_fmin(double min1, double min2);
+int itc_skv(int num);
+int itc_spr(int a, int b);
+int itc_str(int a, int b, int c);
+double itc_scir(int radius);
+double itc_pow(int num, int step);
+bool itc_ispositive(int num);
+bool itc_ispositive_d(double num);
+int itc_sqrt(int num);
+
+#endif 
---
+++ find2.cpp
@@ -0,0 +1,62 @@
+#include "middle.h"
+#include "easy.h"
+#include <iostream>
+using namespace std;
+
+
+int itc_null_count(long long number) {
+	int kol = 0;
+	if (number < 0) {
+		number *= -1;
+	}
+	if (number == 0) {
+		return 1;
+	}
+	while (number > 0) {
+		if (number % 10 == 0) {
+			kol++;
+		}
+		number /= 10;
+	}
+	return kol;
+
+}
+int itc_len_num(long long number) {
+	long long kol = 0;
+	if (number < 0) {
+		number *= -1;
+	}
+	if (number == 0) {
+		return 1;
+	}
+	while (number > 0) {
+		kol++;
+		number /= 10;
+	}
+	return kol;
+}
+int itc_sum_num(long long number) {
+	long long sum = 0;
+	if (number < 0) {
+		number *= -1;
+	}
+	while (number > 0) {
+		sum += number % 10;
+		number /= 10;
+	}
+	return sum;
+}
+long long itc_multi_num(long long number) {
+	long long prois = 1;
+	if (number < 0) {
+		number *= -1;
+	}
+	if (number == 0) {
+		return 0;
+	}
+	while (number > 0) {
+		prois *= number % 10;
+		number /= 10;
+	}
+	return prois;
+}
